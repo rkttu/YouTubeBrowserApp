32e0cd1dbc40630b540f9ce6ae8132330543a2ea
Jeong Hyun Nam <rkttu@rkttu.com>
2019-02-04 15:35:16 +0900

    Add AccuRadio dedicated app

---
+++ AccuRadioBrowserAppCore/NativeMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AccuRadioBrowserApp
+{
+    internal static class NativeMethods
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        public const int WS_SIZEBOX = 0x00040000;
+        public const int WS_MINIMIZEBOX = 0x00020000;
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.Winapi, ExactSpelling = true, CharSet = CharSet.Unicode, EntryPoint = "SendMessageW")]
+        public static extern int SendMessage(IntPtr handle, int message, int wordParameter, int longParameter);
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.Winapi, ExactSpelling = true, CharSet = CharSet.None, EntryPoint = "ReleaseCapture")]
+        public static extern bool ReleaseCapture();
+
+        public enum PROCESS_DPI_AWARENESS : int
+        {
+            PROCESS_DPI_UNAWARE,
+            PROCESS_SYSTEM_DPI_AWARE,
+            PROCESS_PER_MONITOR_DPI_AWARE
+        }
+
+        [DllImport("shcore.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.None, EntryPoint = "SetProcessDpiAwareness")]
+        [return: MarshalAs(UnmanagedType.U4)]
+        public static extern int SetProcessDpiAwareness(PROCESS_DPI_AWARENESS value);
+    }
+}
---
+++ AccuRadioBrowserAppCore/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AccuRadioBrowserApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.OleRequired();
+
+            NativeMethods.SetProcessDpiAwareness(NativeMethods.PROCESS_DPI_AWARENESS.PROCESS_PER_MONITOR_DPI_AWARE);
+
+            SetWebViewFeature("FEATURE_BROWSER_EMULATION", 11000);
+            SetWebViewFeature("FEATURE_96DPI_PIXEL", 1);
+
+            using (var mainForm = new MainForm())
+            {
+                var appContext = new ApplicationContext(mainForm);
+                Application.Run(appContext);
+            }
+        }
+
+        private static void SetWebViewFeature(string featureName, object value)
+        {
+            var targetPath = $@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\{featureName}";
+            var execFileName = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+            var subKey = Registry.CurrentUser.OpenSubKey(targetPath, true);
+
+            if (subKey == null)
+                subKey = Registry.CurrentUser.CreateSubKey(targetPath);
+
+            using (subKey)
+            {
+                subKey.SetValue(execFileName, value);
+            }
+        }
+    }
+}
